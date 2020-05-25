2b75fa00609cedd0e752c9521a29c50d861db9f2
jaeyun95 <wodbs9522@gmail.com>
2020-05-25 15:39:18 +0900

    update

--- extractionKeyword.py
+++ extractionKeyword.py
@@ -9,4 +9,22 @@ class extractionKeyword():
         self.answer_list = instance['answer_list']
         #self.visual_concepts = instance['visual_concpets']
 
+    def _remove_stopword(self):
+        stop_words = set(nltk.corpus.stopwords.words('english'))
+        stop_words.update(['.','\'s','\'',',','?','why','how','what','where','when','yes','no'])
+        self.question = [w.lower() for w in self.question if w.lower() not in stop_words]
+        self.question = list(set(self.question))
+        for i,answer in enumerate(self.answer_list):
+            self.answer_list[i] = [w.lower() for w in answer if w.lower() not in stop_words]
+            self.answer_list[i] = list(set(self.answer_list[i]))
+
     def _prototype(self):
+        lm = nltk.stem.WordNetLemmatizer()
+        self.question = [lm.lemmatize(w, pos="v") for w in self.question]
+        for i,answer in enumerate(self.answer_list):
+            self.answer_list[i] = [lm.lemmatize(w, pos="v") for w in answer]
+
+    def get_keyword(self):
+        self._remove_stopword()
+        self._prototype()
+        return self.question, self.answer_list
--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 from dataloaders.vcr import VCR
+from extractionKeyword import extractionKeyword
 
 instance = VCR('val', 'rationale')
 
 for i,item in enumerate(instance):
-    print(item)
+    keyword = extractionKeyword(item)
+    question, answer_list = keyword.get_keyword()
+    print(question)
