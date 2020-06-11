6ec91d682d82ff4c84788102f7253257d1a51ab5
jaeyun95 <wodbs9522@gmail.com>
2020-06-11 23:21:35 +0900

    update

--- run_vcr.py
+++ run_vcr.py
@@ -8,8 +8,8 @@ import torch
 
 train_answer = VCR('train','answer')
 train_rationale = VCR('train','rationale')
-#val_answer = VCR('val','answer')
-#val_rationale = VCR('val', 'rationale')
+val_answer = VCR('val','answer')
+val_rationale = VCR('val', 'rationale')
 #test_answer = VCR('test','answer')
 #test_rationale = VCR('test', 'rationale')
 
@@ -34,3 +34,14 @@ for t_answer,t_rationale in zip(train_answer,train_rationale):
         #topKExtractor.get_topKknowledge(t_rationale['question'], rationale_list[i])
 #end = time.time() # this is test
 #print('time : ',end-start) # this is test
+
+for v_answer,v_rationale in zip(val_answer,val_rationale):
+    answer_list = []
+    rationale_list = []
+    for i in range(4):
+        answer_list.append(knowledgeExtractor.get_knowledge(keywordExtractor.get_keyword(v_answer['answer_list'][i])))
+        #rationale_list.append(knowledgeExtractor.get_knowledge(keywordExtractor.get_keyword(t_rationale['answer_list'][i])))
+
+    for i in range(4):
+        topKExtractor.get_topKknowledge(v_answer['question'],answer_list[i])
+        #topKExtractor.get_topKknowledge(t_rationale['question'], rationale_list[i])
